76a26b51278f48f833fe0528f012ed0450b02f2b
psj <peterjun0707@naver.com>
2025-05-15 15:22:32 +0900

    'BaseResponse의 정적 펙토리 매서드에 느슨한 결합을 추가해보자'까지 했습니다.

---
+++ src/main/java/study/goorm/global/error/code/status/SuccessStatus.java
@@ -0,0 +1,29 @@
+package study.goorm.global.error.code.status;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+import study.goorm.global.error.code.BaseCode;
+
+@Getter
+@AllArgsConstructor
+public enum SuccessStatus implements BaseCode {
+
+    //Common
+    OK(HttpStatus.OK, "COMMON_200", "성공입니다.");
+
+    private final HttpStatus httpStatus;
+    private final String code;
+    private final String message;
+
+    @Override
+    public String getCode() {
+        return code;
+    }
+
+    @Override
+    public String getMessage() {
+        return message;
+    }
+}
+
