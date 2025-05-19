81939187cb9f0cfb0a590140c2fd1cab8997389c
kuj7373 <kuj7373@naver.com>
2025-05-19 17:36:11 +0900

    과제6 API 응답 처리 과제 완료

--- src/main/java/study/goorm/global/error/code/status/SuccessStatus.java
+++ src/main/java/study/goorm/global/error/code/status/SuccessStatus.java
@@ -24,4 +24,14 @@ public enum SuccessStatus implements BaseCode {
     public String getMessage() {
         return message;
     }
+
+    @Override
+    public ReasonDTO getReasonHttpStatus() {
+        return ReasonDTO.builder()
+                .message(message)
+                .code(code)
+                .isSuccess(true)
+                .httpStatus(httpStatus)
+                .build();
+    }
 }
