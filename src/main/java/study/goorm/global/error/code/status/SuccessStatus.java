ed9770b1a33ebe88fef52d41cde4900f009fd089
hyowon <ally0204@naver.com>
2025-05-19 12:44:58 +0900

    [feat/#26] 에러 핸들링

--- src/main/java/study/goorm/global/error/code/status/SuccessStatus.java
+++ src/main/java/study/goorm/global/error/code/status/SuccessStatus.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.springframework.http.HttpStatus;
 import study.goorm.global.error.code.BaseCode;
+import study.goorm.global.error.code.ReasonDTO;
 
 @Getter
 @AllArgsConstructor
@@ -24,6 +25,16 @@ public enum SuccessStatus implements BaseCode {
     @Override
     public String getMessage() {
         return message;
+    }
+
+    @Override
+    public ReasonDTO getReasonHttpStatus() {
+        return ReasonDTO.builder()
+                .httpStatus(httpStatus)
+                .isSuccess(false)
+                .code(code)
+                .message(message)
+                .build();
     }
 
 }
